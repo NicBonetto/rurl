f37024304447a7a567f78d462843c1d1c90ede84
Nic Bonetto <nick.bonetto@gmail.com>
2018-08-21 22:35:56 -0700

    Defined Url struct.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![feature(use_extern_macros)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[derive(Clone)]
+#[wasm_bindgen]
+pub struct Url {
+  serialized: String,
+  scheme_end: u32,
+  user_end: u32,
+  host_start: u32,
+  host_end: u32,
+  port: Option<u16>,
+  path_start: u32,
+  query_start: Option<u32>,
+  fragment_start: Option<u32>
 }
